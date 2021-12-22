fa99ff7667aa80fa99c651ae711e8bf6e73fa93e
Gonçalo Amaral <goa.docs@gmail.com>
2021-12-22 00:05:28 +0000

    Refactor 03 with golang sdl engine

--- go/03_event_driven_programming/main.go
+++ go/03_event_driven_programming/main.go
@@ -6,40 +6,28 @@
 package main
 
 import (
+	eng "github.com/Goamaral/learning_sdl/engine"
+	win "github.com/Goamaral/learning_sdl/window"
+
 	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
-	// Init SDL
-	err := sdl.Init(sdl.INIT_EVERYTHING)
-	if err != nil {
-		panic(err)
-	}
-	defer sdl.Quit()
+	eng.Init()
+	defer eng.Quit()
 
-	// Create window
-	window, err := sdl.CreateWindow("SDL Tutorial", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 640, 480, sdl.WINDOW_SHOWN)
-	if err != nil {
-		panic(err)
-	}
+	window := win.Create()
 	defer window.Destroy()
 
-	// Main loop
-	running := true
-	for running {
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch eventType := event.(type) {
-			// Window closed
-			case *sdl.QuitEvent:
-				running = false
-
-			// Key pressed
-			case *sdl.KeyboardEvent:
-				switch eventType.Keysym.Sym {
-				case sdl.K_ESCAPE:
-					running = false
-				}
+	eng.Loop(func(event sdl.Event) bool {
+		switch eventType := event.(type) {
+		// Key pressed
+		case *sdl.KeyboardEvent:
+			switch eventType.Keysym.Sym {
+			case sdl.K_ESCAPE:
+				return false
 			}
 		}
-	}
+		return true
+	})
 }
