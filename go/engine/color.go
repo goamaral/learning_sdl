8c2576e9b574d3f0de8cc15ab45a58b4abece6ba
Gonçalo Amaral <goa.docs@gmail.com>
2022-03-22 16:24:15 +0000

    Add more default colors

--- go/engine/color.go
+++ go/engine/color.go
@@ -1,7 +1,13 @@
 package engine
 
 var (
-	COLOR_BLACK = Color{0, 0, 0, 255}
+	COLOR_BLACK   = Color{0, 0, 0, 255}
+	COLOR_RED     = Color{255, 0, 0, 255}
+	COLOR_GREEN   = Color{0, 255, 0, 255}
+	COLOR_BLUE    = Color{0, 0, 255, 255}
+	COLOR_YELLOW  = Color{255, 255, 0, 255}
+	COLOR_MAGENTA = Color{255, 0, 255, 255}
+	COLOR_CYAN    = Color{0, 255, 255, 255}
 )
 
 type Color struct {
